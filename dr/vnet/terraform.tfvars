a53981e69817907337cba41a9b7e4cf5527896f0
Aayush <aayush.singha01@@gmail.com>
2026-03-26 17:18:01 +0530

    add workflow

---
+++ dr/vnet/terraform.tfvars
@@ -0,0 +1,10 @@
+Vnet = {
+  Vnet1 = {
+    name                = "vnet1"
+    location            = "eastus"
+    address_space       = ["10.0.0.0/16"]
+    resource_group_name = "testrgdrhb"
+  }
+}
+
+
---
+++ dr/vnet/variable.tf
@@ -0,0 +1,9 @@
+variable "Vnet" {
+  type = map(object({
+    name                = string
+    location            = string
+    address_space       = list(string)
+    resource_group_name = string
+  }))
+}
+
